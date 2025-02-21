b5579d12d9f3e94a96d5ea9fc9cb868a22dd3629
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-02-21 04:30:16 -0800

    python

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):  # Loop through numbers 0 to 99
-    print("{} = 0x{:x}".format(i, i))  # Print number and its hexadecimal equivalent
+for i in range(100):
+    if i == 99:
+        print(f"{i:02d}")
+    else:
+        print(f"{i:02d}", end=", ")
