56edd776f315ea96e4a2e0473a312a7bc37d5234
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-02-21 03:05:41 -0800

    printed

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = "98"  # string representation of the number
-print(f"{int(number)} Battery street")  # Convert to integer before formatting
+number = "98"  # if it's a string
+print(f"{int(number)} Battery street")
