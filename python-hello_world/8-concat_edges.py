325743a57d926b9ae5919bdbd53c8a56df9b5c0f
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-02-21 03:29:13 -0800

    concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "object-" + "oriented"
+import sys
+str = sys.argv[0][:6] + "object" + " -oriented"
 str1 = "programming"
-str2 = "with" + " Python"
+str2 = "with Python"
 print(str + " " + str1 + " " + str2)
