5c328d8fbc91ec3ea362626d8f9349e14af8d150
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-04 05:58:24 -0800

    power

--- python-if_else_loops_functions/11-main.py
+++ python-if_else_loops_functions/11-main.py
@@ -3,9 +3,8 @@
 
 pow = __import__('11-pow').pow
 
-print(pow(1, 2))
-print(pow(8, 3))
-print(pow(10, 2))
+print(pow(2, 2))
+print(pow(98, 2))
 print(pow(98, 0))
 print(pow(100, -2))
 print(pow(-4, 5))
--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,4 +2,18 @@
 
 
 def pow(a, b):
-    return a ^ b
+    # Case 1: If b is 0, return 1 (since any number to the power of 0 is 1)
+    if b == 0:
+        return 1
+    # Case 2: If b is positive, multiply a by itself b times
+    elif b > 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    # Case 3: If b is negative, return 1 / (a^|b|)
+    else:
+        result = 1
+        for _ in range(abs(b)):
+            result *= a
+        return 1 / result
