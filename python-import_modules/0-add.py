0ffa325c8ea7be3b59ed328d5205ebc92f2bb86c
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-04 06:37:51 -0800

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,5 +4,20 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print(f"{a} + {b} = {add(a, b)}")
+
+    a = 10
+    b = 30
+    print(f"{a} + {b} = {add(a, b)}")
+
+    a = -10
+    b = 30
+    print(f"{a} + {b} = {add(a, b)}")
+
+    a = -10
+    b = -30
+    print(f"{a} + {b} = {add(a, b)}")
+
+    a = 5
+    b = "H"
+    print(f"{a} + {b} = {add(a, b)}")
