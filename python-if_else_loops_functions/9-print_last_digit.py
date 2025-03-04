2ddd73c7d1739236ca492e817674cd0514350a41
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-04 04:15:10 -0800

    last digit

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10  # Get the last digit (absolute value)
+    print(last_digit, end="")  # Print without a newline
+    return last_digit  # Return the last digit
+
