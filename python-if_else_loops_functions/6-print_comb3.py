4b4bdfaaef46f8e2d85c181c2a4ac31a74f28af6
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-04 06:20:39 -0800

    print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,8 +4,9 @@
 for i in range(10):
     # Loop through all possible second digits (j) that are greater than the first digit
     for j in range(i + 1, 10):
-        # Print the combination with a comma and space except for the last combination (8, 9)
+        # If it's the last combination (89), print it without a comma
         if i == 8 and j == 9:
             print(f"{i}{j}")
         else:
+            # For other combinations, print with a comma and space
             print(f"{i}{j}", end=", ")
