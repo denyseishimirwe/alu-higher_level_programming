c01418fceea6008d90f15e53f20754979d9f5e2c
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-05 15:26:49 -0800

    calc

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,8 +5,8 @@ from calculator_1 import add, sub, mul, div  # Import functions
 a = 10  # Define variable a
 b = 5   # Define variable b
 
-print("{} + {} = {}".format(a, b, add(a, b)))  # Addition
-print("{} - {} = {}".format(a, b, sub(a, b)))  # Subtraction
-print("{} * {} = {}".format(a, b, mul(a, b)))  # Multiplication
-print("{} / {} = {}".format(a, b, div(a, b)))  # Division
-
+if __name__ == "__main__":  # Ensure code runs only when executed directly
+    print("{} + {} = {}".format(a, b, add(a, b)))  # Addition
+    print("{} - {} = {}".format(a, b, sub(a, b)))  # Subtraction
+    print("{} * {} = {}".format(a, b, mul(a, b)))  # Multiplication
+    print("{} / {} = {}".format(a, b, div(a, b)))  # Division
