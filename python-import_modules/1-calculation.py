bd7eb9ea5fbbf4448b495c65e19efb2215c2bb4a
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-05 15:23:12 -0800

    calc

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div  # Import functions
+
+a = 10  # Define variable a
+b = 5   # Define variable b
+
+print("{} + {} = {}".format(a, b, add(a, b)))  # Addition
+print("{} - {} = {}".format(a, b, sub(a, b)))  # Subtraction
+print("{} * {} = {}".format(a, b, mul(a, b)))  # Multiplication
+print("{} / {} = {}".format(a, b, div(a, b)))  # Division
+
