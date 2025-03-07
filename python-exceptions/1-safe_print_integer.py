767596f2f539c2a50eea274024435c58de021b9e
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:03:49 -0800

    integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,9 @@
 
 
 def safe_print_integer(value):
-    """Prints an integer using '{:d}'.format() and returns True if successful."""
+    """
+    Prints an integer using '{:d}'.format() and returns True if successful.
+    """
     try:
         print("{:d}".format(value))
         return True
