9707581a167afbcfcffc60ee6e74945da98ad072
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:38:45 -0800

    square

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    pass
+
+# Main part of the script to demonstrate usage
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
