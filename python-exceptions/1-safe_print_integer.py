459359cbd574c310da4a0b351c4cab280a4a2bf2
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:00:54 -0800

    integer

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """Prints an integer using '{:d}'.format() and returns True if successful."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+if __name__ == "__main__":
+    # Test cases
+    values = [89, -89, "School"]
+
+    for value in values:
+        has_been_print = safe_print_integer(value)
+        if not has_been_print:
+            print("{} is not an integer".format(value))
