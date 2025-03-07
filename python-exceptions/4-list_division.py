1e3c8b8e159ebb58e94989fa143def25199534e2
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:21:01 -0800

    list divsion

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,23 +3,18 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result = []
+
     for i in range(list_length):
         try:
-            # Check if both elements are numbers
             if i >= len(my_list_1):
-                raise IndexError("out of range")
+                raise IndexError
             if i >= len(my_list_2):
-                raise IndexError("out of range")
-            
-            el_1 = my_list_1[i]
-            el_2 = my_list_2[i]
-            
-            if not isinstance(el_1, (int, float)) or not isinstance(el_2, (int, float)):
-                raise TypeError("wrong type")
-            
-            # Perform the division
-            result.append(el_1 / el_2)
-        
+                raise IndexError
+            if not isinstance(my_list_1[i], (int, float)):
+                raise TypeError
+            if not isinstance(my_list_2[i], (int, float)):
+                raise TypeError
+            result.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
@@ -33,16 +28,3 @@ def list_division(my_list_1, my_list_2, list_length):
             pass
 
     return result
-
-if __name__ == "__main__":
-    my_l_1 = [10, 8, 4]
-    my_l_2 = [2, 4, 4]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
-    print("--")
-
-    my_l_1 = [10, 8, 4, 4]
-    my_l_2 = [2, 0, "H", 2, 7]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
