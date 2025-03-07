9cdf8353d0175732847f2757003b13cf2bbb8439
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 04:41:23 -0800

    rectangele

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
+
+"""
+Module for defining a Rectangle class.
+"""
+
+
 class Rectangle:
+    """
+    Class representing a rectangle.
+    
+    This class does not define any properties or methods yet.
+    It serves as a basic template for further expansion.
+    """
     pass
