fcba167c187aecab2a4d7618a60294cb4f7b62ba
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:56:33 -0800

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 
+"""
+This module defines a Square class that represents a square shape with a private size attribute.
+It includes methods for object instantiation and controlled access to the size attribute.
+"""
+
+
 class Square:
-
-
     """
-    This class defines a square with a private size attribute.
+    A Square class that defines a square with a private size attribute.
     
     Attributes:
         __size (int): The size of the square.
@@ -13,7 +17,7 @@ class Square:
 
     def __init__(self, size):
         """
-        Initializes the square with a given size.
+        Initializes a new Square instance with a specified size.
         
         Args:
             size (int): The size of the square.
@@ -22,29 +26,32 @@ class Square:
 
     def __str__(self):
         """
-        Returns a string representation of the square object.
+        Returns a string representation of the Square object, showing its size.
+        
+        Returns:
+            str: A string representation of the square with its size.
         """
         return f"Square(size={self.__size})"
 
 if __name__ == "__main__":
-    # Create a Square instance with size 3
+    # Test with a square of size 3
     my_square = Square(3)
-    print(type(my_square))  # Output the type of the square object
-    print(my_square.__dict__)  # Output the internal dictionary of the object
-
-    # Create a Square instance with size 89
+    print(type(my_square))  # Should print the type of the object (Square)
+    print(my_square.__dict__)  # Should print the internal dictionary with private size
+    
+    # Test with another square of size 89
     another_square = Square(89)
-    print(type(another_square))  # Output the type of the square object
-    print(another_square.__dict__)  # Output the internal dictionary of the object
-
-    # Try accessing the 'size' attribute (it should fail)
+    print(type(another_square))  # Should print the type of the object (Square)
+    print(another_square.__dict__)  # Should print the internal dictionary with private size
+    
+    # Try accessing the 'size' attribute (it should fail as it is private)
     try:
         print(my_square.size)
     except Exception as e:
-        print(e)  # Print the exception message
+        print(e)  # Should print the exception message
 
-    # Try accessing the '_size' attribute (it should fail)
+    # Try accessing the '_size' attribute (it should fail as it is private and mangled)
     try:
         print(my_square._size)
     except Exception as e:
-        print(e)  # Print the exception message
+        print(e)  # Should print the exception message
