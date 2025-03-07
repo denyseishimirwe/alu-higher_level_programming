997e6875e984473addfc69e9bd0ec6ee2cec0f93
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:41:18 -0800

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 
 
+"""
+This module defines a Square class.
+The class is empty and does not have any attributes or methods.
+It is used to demonstrate a simple class definition in Python.
+"""
+
 class Square:
+    """
+    A class representing a square.
+    
+    This class does not have any attributes or methods yet.
+    It serves as an example of a basic Python class structure.
+    """
     pass
 
 # Main part of the script to demonstrate usage
 if __name__ == "__main__":
     my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
+    print(type(my_square))  # Will print the type of the object
+    print(my_square.__dict__)  # Will print the object's __dict__ (attributes)
+
