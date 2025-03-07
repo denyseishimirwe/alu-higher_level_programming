03ad7262ae3c3bd3bc5814b6cae21ef36a7f82bb
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 02:53:34 -0800

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,20 +2,49 @@
 
 
 class Square:
+
+
+    """
+    This class defines a square with a private size attribute.
+    
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes the square with a given size.
+        
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
 
-if __name__ == "__main__":
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
+    def __str__(self):
+        """
+        Returns a string representation of the square object.
+        """
+        return f"Square(size={self.__size})"
 
+if __name__ == "__main__":
+    # Create a Square instance with size 3
+    my_square = Square(3)
+    print(type(my_square))  # Output the type of the square object
+    print(my_square.__dict__)  # Output the internal dictionary of the object
+
+    # Create a Square instance with size 89
+    another_square = Square(89)
+    print(type(another_square))  # Output the type of the square object
+    print(another_square.__dict__)  # Output the internal dictionary of the object
+
+    # Try accessing the 'size' attribute (it should fail)
     try:
         print(my_square.size)
     except Exception as e:
-        print(e)
+        print(e)  # Print the exception message
 
+    # Try accessing the '_size' attribute (it should fail)
     try:
-        print(my_square.__size)
+        print(my_square._size)
     except Exception as e:
-        print(e)
+        print(e)  # Print the exception message
