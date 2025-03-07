ec52a9c7deebb615a58504396dd403d5f3fee847
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 03:18:56 -0800

    py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,14 +5,19 @@
 Module to define a Square class with size validation.
 """
 
+
 class Square:
     """Class representing a square with size validation."""
     def __init__(self, size=0):
-        if not isinstance(size, int): raise TypeError("size must be an integer")
-        if size < 0: raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0: 
+            raise ValueError("size must be >= 0")
         self.__size = size
 
 if __name__ == "__main__":
     for val in [3, 0, "3", -89]:
-        try: sq = Square(val)
-        except Exception as e: print(e)
+        try:
+            sq = Square(val)
+        except Exception as e:
+            print(e)
