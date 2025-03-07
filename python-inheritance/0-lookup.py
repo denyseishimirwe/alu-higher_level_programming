b5ae4c2403b2852bdda422dc809af27e2d367706
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-07 14:25:07 -0800

    gonna be hard

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
-"""Returns list of attributes and methods of an object."""
+
+"""Module providing the lookup methods of an object."""
+
 
 def lookup(obj):
-    """
-    Returns a list of attributes and methods of the object.
+    """Returns a list of available attributes.
+
+    Args:
+        obj (any): The object to inspect.
+
+    Returns:
+        list: A list of attribute and method names.
     """
     return dir(obj)
-
-# Example cases
-class MyClass1:
-    """ A simple class """
-    pass
-
-class MyClass2:
-    my_attr1 = 89
-    def one_meth(self):
-        pass
-
-if __name__ == "__main__":
-    # Test cases
-    print(lookup(int))           # Expected: List of attributes and methods of int
-    print(lookup(float))         # Expected: List of attributes and methods of float
-    print(lookup(object))        # Expected: List of attributes and methods of object
-    print(lookup(list))          # Expected: List of attributes and methods of list
-    print(lookup(MyClass1))      # Expected: List of inherited attributes and methods of MyClass1
-    print(lookup(MyClass2))      # Expected: List of attributes and methods of MyClass2
-    print(lookup(MyClass))       # Custom case: ['my_class', 'is', 'empty']
