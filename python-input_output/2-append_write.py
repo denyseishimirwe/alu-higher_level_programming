d1c6b532d93c161959f71f64be3325c6a735aded
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-12 06:33:53 -0700

    append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""
+Module for appending text to a file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to a UTF-8 file and returns the number of characters added.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The text to be appended to the file. 
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
+
+
+# Test case
+if __name__ == "__main__":
+    nb_characters_added = append_write("file_append.txt", "This School is cool!\n")
+    print(nb_characters_added)
