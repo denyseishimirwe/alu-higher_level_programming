28495845d8000c4232f1f3a0c75e6e898528a343
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 01:31:24 -0700

    student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,20 +2,19 @@
 
 
 """
-Student class with public instance attributes and a method to retrieve 
+Student class with public instance attributes and a method to retrieve
 a dictionary representation of the instance.
 """
 
+
 class Student:
     """Define a student with first_name, last_name, and age."""
-
 
     def __init__(self, first_name, last_name, age):
         """Initialize the student with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """Return the dictionary representation of the Student instance."""
