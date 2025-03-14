9005ec330b3903f47ca51df83e274f791a169ba6
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 02:07:09 -0700

    student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -2,7 +2,7 @@
 
 
 """
-Student class with a method to retrieve a filtered dictionary representation 
+Student class with a method to retrieve a filtered dictionary representation
 based on given attributes.
 """
 
@@ -23,5 +23,4 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
-
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
