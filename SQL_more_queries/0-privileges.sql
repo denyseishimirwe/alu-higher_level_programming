f0aa8137d87d87ff05650a71443618e12310800f
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 04:29:09 -0700

    privelege

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,15 +1,7 @@
--- Check if user_0d_1 exists before showing grants
-SELECT 
-  CASE 
-    WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') 
-    THEN SHOW GRANTS FOR 'user_0d_1'@'localhost'
-    ELSE 'There is no such grant defined for user ''user_0d_1'' on host ''localhost'''
-  END;
+-- Check privileges for user_0d_1
+SELECT CONCAT('Grants for user_0d_1@localhost') AS User_Grants;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Check if user_0d_2 exists before showing grants
-SELECT 
-  CASE 
-    WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') 
-    THEN SHOW GRANTS FOR 'user_0d_2'@'localhost'
-    ELSE 'There is no such grant defined for user ''user_0d_2'' on host ''localhost'''
-  END;
+-- Check privileges for user_0d_2
+SELECT CONCAT('Grants for user_0d_2@localhost') AS User_Grants;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
