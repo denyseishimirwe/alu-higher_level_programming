84ac91e5c03cc6bec36834f9e4818232abd24c2f
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 04:45:00 -0700

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,19 +1,4 @@
--- Check for user_0d_1
-SELECT 
-    CASE 
-        WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') 
-        THEN CONCAT('Grants for user_0d_1@localhost') 
-        ELSE 'There is no such grant defined for user ''user_0d_1'' on host ''localhost''' 
-    END AS User_Grants;
-
+-- lists privileges of the MySQL users
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Check for user_0d_2
-SELECT 
-    CASE 
-        WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') 
-        THEN CONCAT('Grants for user_0d_2@localhost') 
-        ELSE 'There is no such grant defined for user ''user_0d_2'' on host ''localhost''' 
-    END AS User_Grants;
-
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
