ce1244fdb68e6e2824dc62a2a4d5cbad8c9c00b7
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 02:59:25 -0700

    database

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,17 @@
--- This script lists all databases on the MySQL server
+#!/bin/bash
+# This script runs the SQL query to list all databases
 
--- Show all databases, sorted in alphabetical order
-SHOW DATABASES
-ORDER BY Database;
+# Show the initial databases
+echo "Correct output: initial databases sorted"
+mysql -hlocalhost -uroot -p < 0-list_databases.sql
+
+# Create multiple databases
+echo "Creating multiple databases..."
+mysql -hlocalhost -uroot -p -e "CREATE DATABASE db1;"
+mysql -hlocalhost -uroot -p -e "CREATE DATABASE db2;"
+mysql -hlocalhost -uroot -p -e "CREATE DATABASE db3;"
+
+# Show the databases after creation of new databases
+echo "Correct output: after creation of multiple databases"
+mysql -hlocalhost -uroot -p < 0-list_databases.sql
+
