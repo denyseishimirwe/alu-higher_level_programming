b77bdc2004b9b16f28b88f7352327fc64055cd3e
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 04:35:51 -0700

    uswer

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Check if user already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Reload the privileges to ensure the changes take effect
+FLUSH PRIVILEGES;
