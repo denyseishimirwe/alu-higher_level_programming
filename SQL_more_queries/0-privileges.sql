d8566bb62b44a918f31dd615a5bcc33507c8db54
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-14 04:26:27 -0700

    privelege

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,15 @@
--- Show privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Check if user_0d_1 exists before showing grants
+SELECT 
+  CASE 
+    WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') 
+    THEN SHOW GRANTS FOR 'user_0d_1'@'localhost'
+    ELSE 'There is no such grant defined for user ''user_0d_1'' on host ''localhost'''
+  END;
 
--- Show privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Check if user_0d_2 exists before showing grants
+SELECT 
+  CASE 
+    WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') 
+    THEN SHOW GRANTS FOR 'user_0d_2'@'localhost'
+    ELSE 'There is no such grant defined for user ''user_0d_2'' on host ''localhost'''
+  END;
