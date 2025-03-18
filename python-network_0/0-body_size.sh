934bcff861b54c38752bdba2e859467545d9d544
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-18 09:35:26 +0000

    network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL and displays the size of the response body in bytes
-curl -s "$1" | awk '{print length}'
+# This script sends a GET request to the URL and displays the body of the response
+curl -s "$1" | awk '{print "GET / => \"" $0 "\""}'
