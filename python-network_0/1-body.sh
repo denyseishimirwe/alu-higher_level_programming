c96d2fdb5de37883090b68f89ba8593dfaf0744c
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-18 09:53:36 +0000

    body

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request to URL and displays only the body if status code is 200
-curl -s -I "$1" | grep "HTTP/" | grep -q "200 OK" && curl -s "$1"
+curl -sL -o /tmp/curl_body -w "%{http_code}" "$1" | grep -q "200" && cat /tmp/curl_body
