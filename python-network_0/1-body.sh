44d3c69e996fb32b3cb2fd34f5bbe54ff3ea8a1b
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-18 09:40:19 +0000

    body

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL and displays the body of the response if the status code is 200
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+# This script sends a GET request to the URL, follows redirections, and displays the body of the response for a 200 status code
+curl -Ls "$1" -w "%{http_code}" -o response_body.txt | grep -q "200" && cat response_body.txt
