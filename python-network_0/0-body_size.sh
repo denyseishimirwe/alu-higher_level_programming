5e013b8f6421caaa6a7d3cb75ed51a1d67e0ccd9
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-18 09:25:14 +0000

    network

---
+++ python-network_0/0-body_size.sh
@@ -0,0 +1,8 @@
+heck if a URL is passed as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request and display the size of the response body in bytes
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
