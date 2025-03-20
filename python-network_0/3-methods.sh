c3437b65e3262fc164690e4f76b32583d0b81921
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-03-20 08:48:31 +0000

    sh

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i "allow" | awk '{print $2}'
+# Script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep -i "Allow:" | cut -d " " -f2- | tr -d '\r'
--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request with a custom header and displays the response body
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
