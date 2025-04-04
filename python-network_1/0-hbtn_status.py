a889f9a968525c0a5d6c3dce54e3bd8f0f58875d
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-04-04 06:26:29 +0000

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 
+
 """
-Fetches the status from a given URL using urllib.
+Fetches the status from a given URL using urllib with error handling.
 """
 import urllib.request
+import urllib.error
 
 
 def fetch_status(url):
-    """Fetches and prints the status from the given URL."""
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode("utf-8"))
+    """Fetches and prints the status from the given URL, handling errors."""
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type:", type(body))
+            print("\t- content:", body)
+            print("\t- utf8 content:", body.decode("utf-8"))
+    except urllib.error.URLError as e:
+        print("Error: Unable to fetch the URL.")
+        print("Reason:", e.reason)
 
 if __name__ == "__main__":
-    fetch_status("http://0.0.0.0:5050/status")
+    fetch_status("https://intranet.hbtn.io/status")
